32540db380583f36b36ed3b6fd1e081ccf33bb3b
roy yair <roy.yair@gmail.com>
2019-05-29 22:13:52 +0300

    init project + add functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,145 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import OutlinedInput from '@material-ui/core/OutlinedInput'
+import qs from 'query-string';
+import moment from 'moment';
+
+import './App.scss';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { sortType: 'fdfsdf' };
+
+    this.initSearchParams();
+  }
+
+  initSearchParams() {
+    const oldSearchParams = this.props.store.gallery.searchParams;
+    const newSearchParams = qs.parse(this.props.location.search);
+
+    this.props.store.gallery.searchParams = { ...oldSearchParams, ...newSearchParams };
+  }
+
+  formatDate = (date) => {
+    return moment(date).format('DD/MM/YYYY');
+  };
+
+  search = (e) => {
+    const { value } = e.target;
+
+    this.props.store.gallery.setq(value);
+  };
+
+  handleChange = (e) => {
+    this.props.store.gallery.sortString = e.target.value;
+  };
+
+  render() {
+    let { photos }  = this.props.store.gallery;
+    let numOfPhotos = this.state.page * 9;
+
+    let photosUi = photos.map((photo) =>
+      <Grid key={photo.id} item>
+        <Card style={{ width: '300px' }}>
+          <CardContent>
+            <Typography component="p">
+              {photo.slug}
+            </Typography>
+          </CardContent>
+          <CardMedia
+            style={{ height: '300px' }}
+            image={photo.images.original.url}
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography component="p">
+              {this.formatDate(photo.import_datetime)}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+
+    return (
+      <div className="App">
+        <Grid container justify="center" spacing={3}>
+          <Grid item xs={12} sm={10} md={8} lg={5} xl={3}>
+            <TextField
+              style={{ width: '100%' }}
+              id="outlined-search"
+              label="Search photos"
+              type="search"
+              className=""
+              margin="normal"
+              variant="outlined"
+              onChange={(e) => this.search(e)}
+            />
+          </Grid>
+        </Grid>
+        {this.props.store.gallery.searchParams.q}
+        <Grid container justify="center" spacing={3}>
+          <Grid item xs={12} sm={10} md={8} lg={5} xl={3}>
+
+            <FormControl variant="outlined">
+
+              <InputLabel ref={this.sortType} htmlFor="outlined-age-simple">
+                sort
+              </InputLabel>
+
+              <Select
+                value={this.props.store.gallery.sortString}
+                onChange={this.handleChange}
+                input={<OutlinedInput labelWidth={1} name="age"
+                                      id="outlined-age-simple"/>}
+              >
+                <MenuItem value={'Sort by Title, ASC'}>Sort by Title,
+                  ASC</MenuItem>
+                <MenuItem value={'Sort by Title DESC'}>Sort by Title
+                  DESC</MenuItem>
+                <MenuItem value={'Sort by Imported date ASC'}>Sort by Imported
+                  date ASC</MenuItem>
+                <MenuItem value={'Sort by Imported date DESC'}>Sort by Imported
+                  date DESC</MenuItem>
+              </Select>
+            </FormControl>
+
+          </Grid>
+        </Grid>
+
+        <Grid container justify="center" spacing={3}>
+          <Grid item>
+            <Typography variant="h6" gutterBottom>
+              displaying <span
+              style={{ fontWeight: 600 }}>{photosUi.length}</span> images
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Grid container justify="center" spacing={3}>
+          {photosUi}
+        </Grid>
+
+        <Grid container justify="center" spacing={3}>
+          <Grid item>
+
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default inject('store')(observer(App));
---
+++ src/store/gallery.js
@@ -0,0 +1,146 @@
+import { shuffle } from 'lodash';
+import {
+  decorate,
+  observable,
+  computed,
+  action,
+  toJS,
+  configure,
+  runInAction
+} from "mobx";
+import axios from 'axios'
+import { sortBy, cloneDeep } from 'lodash';
+import moment from 'moment';
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import qs from 'query-string';
+import React from "react";
+
+const DEFAULT_SORT_TYPE   = 'TITLE';
+const DEFAULT_SORT_VALUE  = 'ASC';
+const DEFAULT_SORT_STRING = 'Sort by Title, ASC';
+
+configure({ enforceActions: 'observed' });
+
+class Gallery {
+  constructor() {
+    this._photos      = [];
+    this.searchParams = {
+      sortType: DEFAULT_SORT_TYPE,
+      sortValue: DEFAULT_SORT_VALUE,
+      q: ''
+    };
+    this.q            = '';
+    this._sortString  = DEFAULT_SORT_STRING;
+  }
+
+  setq(str) {
+    this.q = str;
+  }
+
+  search() {
+    console.log('Search()');
+    axios
+      .get(`http://api.giphy.com/v1/gifs/search?api_key=dCpV0z0dW988CrZDZ8DYJtLMrJJI0pSz&q=${this.q}&limit=9`)
+      .then((res) => {
+        runInAction(() => {this.photos = res.data.data});
+      });
+  }
+
+  get sortString() {
+    return this._sortString;
+  };
+
+  set sortString(str) {
+    switch (str) {
+      case 'Sort by Title, ASC':
+        this.searchParams.sortValue = 'ASC';
+        this.searchParams.sortType  = 'TITLE';
+        break;
+
+      case 'Sort by Title DESC':
+        this.searchParams.sortValue = 'DESC';
+        this.searchParams.sortType  = 'TITLE';
+        break;
+
+      case 'Sort by Imported date ASC':
+        this.searchParams.sortValue = 'ASC';
+        this.searchParams.sortType  = 'DATETIME';
+        break;
+
+      case 'Sort by Imported date DESC':
+        this.searchParams.sortValue = 'DESC';
+        this.searchParams.sortType  = 'DATETIME';
+        break;
+    }
+
+    this._sortString = str;
+  };
+
+  set photos(arr) {
+    this._photos = arr;
+  }
+
+  get photos() {
+
+    switch (this.searchParams.sortType) {
+      case 'TITLE':
+        return this.sortByTitle(this._photos);
+        break;
+
+      case 'DATETIME':
+        return this.sortByDate(this._photos);
+        break;
+
+      default:
+        return this._photos;
+    }
+
+  };
+
+  sortByTitle(arr) {
+    return arr.slice().sort(this.compareByTitle);
+  };
+
+  sortByDate(arr) {
+    return arr.slice().sort(this.compareByDate);
+  };
+
+  compareByTitle = (a, b) => {
+
+    if (a.slug < b.slug) {
+      return this.searchParams.sortValue === 'ASC' ? -1 : 1;
+    }
+
+    if (a.slug > b.slug) {
+      return this.searchParams.sortValue === 'ASC' ? 1 : -1;
+    }
+
+    return 0;
+  };
+
+  compareByDate = (a, b) => {
+    if (moment(a.import_datetime).isBefore(b.import_datetime)) {
+      return this.searchParams.sortValue === 'ASC' ? -1 : 1;
+    }
+
+    if (moment(b.import_datetime).isBefore(a.import_datetime)) {
+      return this.searchParams.sortValue === 'ASC' ? 1 : -1;
+    }
+
+    return 0;
+  }
+}
+
+decorate(Gallery, {
+  _photos: observable,
+  _sortString: observable,
+  sortString: computed,
+  photos: computed,
+  searchParams: observable,
+  q: observable,
+  search: action,
+  setq: action
+});
+
+export default Gallery;
